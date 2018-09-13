a81e97f3cb4c41d8d4d8a5b803fa8c2c58f22a30
karim <beiranvand.karim@gmail.com>
2018-09-13 21:22:24 +0430

    init commit

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
+
+//( )	{ }	[ ]
+//Round brackets or parentheses	Curly brackets or braces	Square brackets or simply brackets (US)
